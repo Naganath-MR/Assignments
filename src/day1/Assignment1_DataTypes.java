cf286c87241b4568006a69340c23241eca54b732
Your Name <you@example.com>
2025-04-09 07:55:36 +0530

    Day1- Assignment submitted

--- src/day1/Assignment1_DataTypes.java
+++ src/day1/Assignment1_DataTypes.java
@@ -4,8 +4,123 @@ public class Assignment1_DataTypes {
 
 	public static void main(String[] args) {
 	
+		// To store Temperature of a city in degrees Celsius: 25.5
 		
-		System.out.println("This is my first assignment");
+		float Temperature_of_city =25.5f;
+		System.out.println("Temperature in Madurai : "+Temperature_of_city +" °C");
+		
+		//To store Whether a customer has placed an order: true or false
+		
+		boolean placed_order = true;
+		System.out.println("Whether a customer has placed an order: " +placed_order);
+		
+		//To store Person's phone number: "123-456-7890"
+		
+         String phone_number = "123-456-7890";
+         System.out.println("Person's phone number: "+phone_number);
+         
+         //To store Amount of money in a customer's bank account: 1000.50
+         
+         float money = 1000.50f;
+         System.out.println("Amount of money in a customer's bank account: "+money);
+        
+         //To store Person's email address: "john.doe@example.com"
+         
+         String Email = "john.doe@example.com";
+         System.out.println("Person's email address: "+Email);
+         
+         //To store Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+         
+         String Coordinates = "37.7749, -122.4194";
+         System.out.println("Coordinates of a location (latitude, longitude): "+Coordinates);
+         
+         //To store Person's marital status:
+         
+         boolean Marital_status =false;
+         System.out.println("Person's marital status: "+Marital_status);
+         
+         //To store Person's occupation: "Software Engineer"
+         String Occupation=  "Software Engineer";
+         System.out.println("Person's occupation: "+Occupation);
+         
+         //To store Person's favourite colour: "Blue"
+         
+         String favourite_colour=  "Blue";
+         System.out.println("Person's favourite colour: "+favourite_colour);
+         
+         //To store Current year: 2025
+         
+         int Current_year =2025;
+         System.out.println("Current year: "+Current_year);
+         
+         //To store Number of followers on a social media platform: 1,000,000
+         
+         String Number_of_followers= "1,000,000";
+         System.out.println("Number of followers on a social media platform: "+Number_of_followers);
+         
+         // To store Rating of a movie: 7.5
+         
+         float Rating=9.5f;
+         System.out.println("Rating of a movie (Dragon): "+Rating+ "/10");
+         
+         
+         //To store Person's blood type: 'A'
+         char blood_type = 'A';
+         System.out.println("Person's blood type: "+blood_type+ " +ve");
+         
+         //To store Title of a book: "To Kill a Mockingbird"
+         
+         String Title= "To Kill a Mockingbird";
+         System.out.println("Title of a book: "+Title);
+         
+         //To store Number of employees in a company: 500
+         
+         int employees = 500;
+         System.out.println("Number of employees in a company: "+employees);
+         
+         
+         // To store Time of an event: 2:30 PM
+         String time = " 2:30 PM ";
+         System.out.println("Time of an event: "+time);
+         
+         //To store Name of a country: "United States"
+         
+         String Country = "United States";
+         System.out.println("Name of a country: "+Country);
+         
+          //To store Person's eye color: "Brown"
+         
+         String color = "Brown";
+         System.out.println("Person's eye color: "+color);
+         
+         
+         //To store Person's birthplace: "New York City"
+         String birthplace = "New York City";
+         System.out.println("Person's birthplace: "+birthplace);
+         
+         //To store Distance between two cities: 200.5
+         
+         float distance = 200.5f;
+         System.out.println("Distance between two cities: "+distance+ " Km");
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
 	}
 
 }
